99a9964f2566a8c6aabff752ed29b738379dbec8
Leonel Maximo Vargas Ramirez <maxramirez@MacBook-Pro-de-Leonel.local>
2024-01-22 18:01:38 -0400

    modificaciones de rutas

--- src/components/keyboard.tsx
+++ src/components/keyboard.tsx
@@ -17,29 +17,39 @@ export const KeyboardSimple = forwardRef((props: keyboardProps, ref) => {
 
   const keyboard = useRef<any>();
 
+  const buttonWidth = keyboardComplete ? "100%" : "70%";
+
+  const buttonTheme = [
+    {
+      class: "hg-green",
+      buttons: `1:${buttonWidth} 2:${buttonWidth} 3:${buttonWidth} 4:${buttonWidth} 5:${buttonWidth} 6:${buttonWidth} 7:${buttonWidth} 8:${buttonWidth} 9:${buttonWidth} 0:${buttonWidth} -:${buttonWidth} {bksp}:${buttonWidth} Q:${buttonWidth} W:${buttonWidth} E:${buttonWidth} R:${buttonWidth} T:${buttonWidth} Y:${buttonWidth} U:${buttonWidth} I:${buttonWidth} O:${buttonWidth} P:${buttonWidth} A:${buttonWidth} S:${buttonWidth} D:${buttonWidth} F:${buttonWidth} G:${buttonWidth} H:${buttonWidth} J:${buttonWidth} K:${buttonWidth} L:${buttonWidth} Ñ:${buttonWidth} Z:${buttonWidth} X:${buttonWidth} C:${buttonWidth} V:${buttonWidth} B:${buttonWidth} N:${buttonWidth} M:${buttonWidth} -:${buttonWidth}`,
+    },
+  ];
+
+  // const buttonTheme = [
+  //   {
+  //     class: "hg-green",
+  //     buttons: `1 2 3 4 5 6 7 8 9 0 - {bksp} Q W E R T Y U I O P A S D F G H J K U L Ñ Z X C V B N M -`,
+  //   },
+  // ];
+
   return (
-    <Keyboard
-      keyboardRef={(r) => (keyboard.current = r)}
-      onChange={onChange}
-      theme={"hg-theme-default hg-layout-default myTheme"}
-      layout={{
-        default: keyboardComplete
-          ? [
-            "1 2 3 4 5 6 7 8 9 0",
-            "Q W E R T Y U I O P",
-            "A S D F G H J K L Ñ",
-            "Z X C V B N M - {bksp}",
-          ]
-          : ["1 2 3", "4 5 6", "7 8 9", "0 {bksp}"],
-      }}
-      buttonTheme={[
-        {
-          class: "hg-green",
-          buttons:
-            "1 2 3 4 5 6 7 8 9 0 - {bksp} Q W E R T Y U I O P A S D F G H J K L Ñ Z X C V B N M",
-        },
-      ]}
-      display={{ '{bksp}': '←' }}
-    />
+      <Keyboard
+        keyboardRef={(r) => (keyboard.current = r)}
+        onChange={onChange}
+        theme={"hg-theme-default hg-layout-default myTheme"}
+        layout={{
+          default: keyboardComplete
+            ? [
+              "1 2 3 4 5 6 7 8 9 0",
+              "Q W E R T Y U I O P",
+              "A S D F G H J K L Ñ",
+              "Z X C V B N M - {bksp}",
+            ]
+            : ["1 2 3", "4 5 6", "7 8 9", "0 {bksp}"],
+        }}
+        buttonTheme={buttonTheme}
+        display={{ '{bksp}': '←' }}
+      />
   );
 });
--- src/views/auth/Auth.tsx
+++ src/views/auth/Auth.tsx
@@ -1,4 +1,4 @@
-import { AppBar, Toolbar } from '@mui/material';
+import { AppBar, Toolbar, Typography } from '@mui/material';
 
 import { IdentityCard } from './IdentityCard';
 // import { RecognitionView } from './recognition';
@@ -21,18 +21,25 @@ export const AuthView = () => {
 
   useEffect(() => {
     let interval: NodeJS.Timeout;
-
+    console.log("esto es identityCard", identityCard)
     if (identityCard != '' && timer > 0) {
+    // if ( timer > 0) {
       interval = setInterval(() => {
         changeTimer(timer - 1);
         if (timer == 1) {
+          console.log("timer es igual a ", 1)
           reconigtionViewRef.current?.onRemoveCam();
           changeIdentityCard('')
           changeIdentifyUser(false)
           changeStateInstruction(true)
           changeTimer(20)
+          changeStep('home')
         }
       }, 1000);
+    } else if (identityCard != '') {
+      changeStep('home')
+      changeIdentityCard('')
+      changeTimer(20)
     }
     return () => clearInterval(interval);
 
@@ -54,6 +61,7 @@ export const AuthView = () => {
       {step != 'home' && <AppBar position="static" style={{ background: '#008698' }}>
         <Toolbar sx={{ py: .5 }}>
           <img src={imageLogoBlanco} alt="Descripción de la imagen" style={{ width: '10vw' }} />
+          <Typography variant='h4' color='white'>{timer}</Typography> 
         </Toolbar>
       </AppBar>}
       {
@@ -63,15 +71,10 @@ export const AuthView = () => {
         step == 'identityCard' && <IdentityCard />
       }
       {
-        step == 'instructionCard' && <InstructionCard onPressed={handlePressedInstructionCard} />
+        identityCard != '' && step == 'instructionCard' && <InstructionCard onPressed={handlePressedInstructionCard} />
       }
       {
-        step == 'recognitionCard' &&
-        // identityCard != '' && (
-          // InstructionState ?
-            // <InstructionCard onPressed={handlePressedInstructionCard} /> :
-            <RecognitionView ref={reconigtionViewRef} />
-        // )
+        step == 'recognitionCard' && <RecognitionView ref={reconigtionViewRef} />
       }
     </>
   );
