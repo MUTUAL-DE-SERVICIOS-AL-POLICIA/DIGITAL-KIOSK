a313ec9d514644f80a1f596fc0278de3af8d186a
MaxRamirez <leorexx100@gmail.com>
2024-11-27 11:36:35 -0400

    Eliminando del index.ts el componente Tab

--- src/components/index.ts
+++ src/components/index.ts
@@ -5,5 +5,4 @@ export * from "./ImageCanvas";
 export * from "./Alphanumeric";
 export * from "./keyboardNumeric";
 export * from "./keyboardAlphanumeric";
-export * from "./Tab";
 export * from "./Card";
