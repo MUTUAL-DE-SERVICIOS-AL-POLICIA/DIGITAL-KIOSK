11bcee24f87120365dafdffffdd146b488061f92
MaxRamirez <leorexx100@gmail.com>
2024-11-28 15:00:54 -0400

    Preparando api para consumir rutas del PVT-BE

--- src/services/coffeApi.ts
+++ src/services/coffeApi.ts
@@ -6,8 +6,8 @@ const {
   VITE_HOST_BACKEND_PRINT,
   VITE_HOST_BACKEND_BIOMETRIC,
   VITE_HOST_API_GATEWAY,
-  // VITE_HOST_API_PVT_BE,
-  // VITE_API_PVT_BE_TOKEN
+  VITE_HOST_API_PVT_BE,
+  VITE_API_PVT_BE_TOKEN,
 } = getEnvVariables();
 
 const createAxiosInstance = (baseURL: string) => {
@@ -16,12 +16,12 @@ const createAxiosInstance = (baseURL: string) => {
   });
 
   instance.interceptors.request.use((request) => {
-    // if(baseURL == VITE_HOST_API_PVT_BE) {
-    //     request.headers.set('Authorization', `Bearer ${VITE_API_PVT_BE_TOKEN}`)
-    // } else {
-    const token = localStorage.getItem(`token`);
-    if (token) request.headers.set("Authorization", `Bearer ${token}`);
-    // }
+    if (baseURL == VITE_HOST_API_PVT_BE) {
+      request.headers.set("Authorization", `Bearer ${VITE_API_PVT_BE_TOKEN}`);
+    } else {
+      const token = localStorage.getItem(`token`);
+      if (token) request.headers.set("Authorization", `Bearer ${token}`);
+    }
     return request;
   });
 
@@ -32,4 +32,4 @@ export const coffeApi = createAxiosInstance(VITE_HOST_BACKEND);
 export const externalApi = createAxiosInstance(VITE_HOST_BACKEND_PRINT);
 export const biometricApi = createAxiosInstance(VITE_HOST_BACKEND_BIOMETRIC);
 export const gatewayApi = createAxiosInstance(VITE_HOST_API_GATEWAY);
-// export const pvtbeApi = createAxiosInstance(VITE_HOST_API_PVT_BE);
+export const pvtbeApi = createAxiosInstance(VITE_HOST_API_PVT_BE);
